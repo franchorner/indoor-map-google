56dde60d9a031b3142651891fd5272abc46bbe47
fran_chorner <francinegonsalves@sou.faccat.br>
2022-04-28 14:39:14 -0300

    mapa funcionando

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width,
+  },
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: -23.591392169792996,
+          longitude: -46.689828646073224,
+          latitudeDelta: 0.0015,
+          longitudeDelta: 0.0015,
         }}
-      />
+        zoomEnabled={true}
+        minZoomLevel={17}
+      >
+      </MapView>
     </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-}); 
+  )
+};
